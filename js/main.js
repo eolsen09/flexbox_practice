910d421335df09ae37afd9a40fd75f41fad79306
Erika Olsen <erika.olsen09@gmail.com>
2018-03-19 18:54:42 -0500

    added javascript to teachers page. printed teacher, department, ratings, and avgRating for Physics teacher

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log('this file is linked!');
+var teacher1 = 'Teacher: Kinsey Oetjen';
+console.log(teacher1);
+
+var department1 = 'Department: Physics';
+console.log(department1);
+
+var ratings = 'Ratings: 3.4, 5.0, 4.2';
+console.log(ratings);
+
+var avgRating = (3.4 + 5.0 + 4.2)/3;
+console.log(avgRating);
