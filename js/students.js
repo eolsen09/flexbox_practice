69ebbfe4b78c106710fba38186fa0db0ef4185da
Erika Olsen <erika.olsen09@gmail.com>
2018-03-19 21:30:28 -0500

    linked js page to courses and student html file. created variables for students page

---
+++ js/students.js
@@ -0,0 +1,16 @@
+console.log('this file is linked!');
+
+var student1 = 'Student: Madeline Carr';
+console.log(student1);
+
+var major1 = 'Major: World Religion';
+console.log(major1);
+
+var maddyEmail = 'Email: maddog@cg.edu';
+console.log(maddyEmail);
+
+var maddyGpa = 'GPA: 3.0';
+console.log(maddyGpa);
+
+var maddyCourses = "Courses: Buddhism, Hinduism";
+console.log(maddyCourses);
