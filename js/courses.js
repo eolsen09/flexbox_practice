348e1e876865d83160b555c52c47cf8a36ba699f
Erika Olsen <erika.olsen09@gmail.com>
2018-03-19 21:34:26 -0500

    created classes, teacher, and semester variables in js file for courses page

---
+++ js/courses.js
@@ -0,0 +1,10 @@
+console.log('this file is linked!');
+
+var classes1 = 'Courses: Astronomy, Physics';
+console.log(classes1);
+
+var teacher1 = "Teacher: Kinsey Oetjen";
+console.log(teacher1);
+
+var semester1 = "Semester: Fall 2018";
+console.log(semester1);
