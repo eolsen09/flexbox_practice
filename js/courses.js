476d3c4cf8bd20aacb98b9692ff47b59e5a8e1d6
Erika Olsen <erika.olsen09@gmail.com>
2018-04-23 19:46:06 -0500

    added prompt for average rating, changed semesters on courses html page, created double array

--- js/courses.js
+++ js/courses.js
@@ -8,3 +8,24 @@ console.log(teacher1);
 
 var semester1 = "Semester: Fall 2018";
 console.log(semester1);
+
+var fallCourses = [
+  ["Astronomy", "Science"],
+  ["Biology", "Life Science"],
+  ["web Development", "Computer Science"],
+  ["Geometry", "Mathematics"],
+  ["Buddism", "World Religions"]
+];
+
+
+function pickScience(courses) {
+  for (var i = 0; i < courses.length; i++) {
+    var currentCourse = courses[i]
+    var department = currentCourse[1]
+    if (department == "Science") {
+      console.log("Science!");
+    }
+  }
+}
+
+pickScience(fallCourses)
--- js/main.js
+++ js/main.js
@@ -1,20 +1,69 @@
 console.log('this file is linked!');
-var teacher1 = 'Teacher: Kinsey Oetjen';
-console.log(teacher1);
+var teacher1 = 'Kinsey Oetjen';
+console.log('Teacher: ' + teacher1);
 
-var department1 = 'Department: Physics';
-console.log(department1);
+var department1 = 'Physics';
+console.log('Department: ' + department1);
 
 var oetjenRating = [3.4, 5.0, 4.2];
 
-var getRatingAvg = oetjenRating.reduce((total, oetjenRating) => total + oetjenRating) / oetjenRating.length;
+function getRatingAvg(ratings){
+  console.log(ratings);
+  const sum = ratings.reduce((a,b) => a + b, 0);
+  console.log(sum);
+  return (sum / ratings.length);
+}
 
-console.log(getRatingAvg);
+console.log(getRatingAvg(oetjenRating));
 
-console.log(oetjenRating.push(5.0));
+var teacher2 = 'Michelle Reuther';
+console.log('Teacher: ' + teacher2);
 
-console.log(oetjenRating);
+var department2 = 'Science';
+console.log('Department: ' + department2);
 
-var getRatingAvg = oetjenRating.reduce((total, oetjenRating) => total + oetjenRating) / oetjenRating.length;
+var reutherRating = [3.5, 3.5, 3.5];
 
-console.log(getRatingAvg);
+console.log(getRatingAvg(reutherRating));
+
+var teacher3 = 'Lucia Brodribb';
+console.log('Teacher: ' + teacher3);
+
+var department3 = 'Mathematics';
+console.log('Department: ' + department3);
+
+var brodribbRating = [4.2, 4.2, 4.2];
+
+console.log(getRatingAvg(brodribbRating));
+
+var teacher4 = 'Erika Olsen';
+console.log('Teacher: ' + teacher4);
+
+var department4 = 'Computer Science';
+console.log('Department: ' + department4);
+
+var olsenRating = [4.0, 4.0, 4.0];
+
+console.log(getRatingAvg(olsenRating));
+
+var teacher5 = 'Bridgett Johnson';
+console.log('Teacher: ' + teacher5);
+
+var department5 = 'World Religions';
+console.log('Department: ' + department5);
+
+var johnsonRating = [3.9, 3.9, 3.9];
+
+console.log(getRatingAvg(johnsonRating));
+
+function addTeacherRating (ratings, newRating) {
+  ratings.push(newRating);
+}
+
+do {
+  var userRating = window.prompt('We would like for you to review Erika Olsen. Please enter a rating between 0.0 - 5.0')
+}
+while (!(userRating >= 0.0 && userRating <= 5.0));
+
+addTeacherRating (olsenRating, userRating)
+alert ('Thank you for your review! ' + teacher4 + ' average rating is now ' + getRatingAvg(olsenRating) + '.');
